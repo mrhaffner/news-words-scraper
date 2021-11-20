4be9063b6aa22853fd4da25527947f6902d23b33
mrhaffner <mrhaffner13@gmail.com>
2021-11-19 18:32:33 -0600

    feat: screated def that intakes a url of an rss feed in feedburner format, and output an object with parsed data

--- main.py
+++ main.py
@@ -2,24 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = 'http://rss.cnn.com/rss/cnn_topstories.rss'
+cnn_url = 'http://rss.cnn.com/rss/cnn_topstories.rss'
+breitbart_url = 'https://feeds.feedburner.com/breitbart'
+fox_url = 'http://feeds.foxnews.com/foxnews/latest'
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+def parse_stories(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    stories = soup.find_all('item')
 
-stories = soup.find_all('item')
+    stories_data = []
 
-stories_data = []
+    for story in stories:
+        story_data = {}
+        story_data['title'] = story.title.get_text()
+        story_data['url'] = story.guid.get_text()
+        dirty_desciption = story.description.get_text()
+        description = dirty_desciption.split('<')[0]
+        story_data['description'] = description
+        story_data['date_collected'] = datetime.today().strftime('%Y-%m-%d')
+        stories_data.append(story_data)
 
-for story in stories:
-    story_data = {}
-    story_data['title'] = story.title.get_text()
-    story_data['url'] = story.guid.get_text()
-    dirty_desciption = story.description.get_text()
-    description = dirty_desciption.split('<')[0]
-    story_data['description'] = description
-    story_data['date_collected'] = datetime.today().strftime('%Y-%m-%d')
-    stories_data.append(story_data)
-
-
-print(stories_data)
+cnn_data = parse_stories(cnn_url)
+breitbart_data = parse_stories(breitbart_url)
+fox_data  = parse_stories(fox_url)
