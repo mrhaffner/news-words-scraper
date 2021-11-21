dd23c159b47557a919513433f49faa6a25089744
mrhaffner <mrhaffner13@gmail.com>
2021-11-20 20:57:35 -0600

    refactor: removed unnecesary pandas dataframe conversion, replaced with tuple unpacking websites query

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 import sqlite3
-import pandas as pd
 
 
 def get_soup(url):
@@ -15,7 +14,7 @@ def get_soup(url):
     return BeautifulSoup(page.content, 'html.parser')
 
 
-def parse_article(url, tag, entry_class, entry_id):
+def scrape_article(url, tag, entry_class, entry_id):
     soup = get_soup(url)
 
     if entry_id is not None:
@@ -25,7 +24,7 @@ def parse_article(url, tag, entry_class, entry_id):
     return article.get_text() if article else ''
 
 
-def parse_rss_feed(url, tag, entry_class, entry_id):
+def scrape_rss_feed(url, tag, entry_class, entry_id):
     soup = get_soup(url)
     articles = soup.find_all('item')
     articles_data = []
@@ -35,7 +34,7 @@ def parse_rss_feed(url, tag, entry_class, entry_id):
         article_data['title'] = article.title.get_text(strip=True)
         article_url = article.guid.get_text(strip=True)
         article_data['url'] = article_url
-        article_data['body'] = parse_article(article_url, tag, entry_class, entry_id)
+        article_data['body'] = scrape_article(article_url, tag, entry_class, entry_id)
         article_data['date_collected'] = datetime.today().strftime('%Y-%m-%d')
 
         articles_data.append(article_data)
@@ -48,22 +47,13 @@ def parse_rss_feed(url, tag, entry_class, entry_id):
 con = sqlite3.connect('website_data.sqlite3')
 cur = con.cursor()
 
-# for row in cur.execute('SELECT id FROM websites'):
-#     print(row[0])
-query = 'SELECT id, rss_url, article_entry_element, article_entry_class, article_entry_id FROM websites'
-SQL_Query = pd.read_sql_query(query, con, index_col=['id'])
+websites_query = 'SELECT id, rss_url, article_entry_element, article_entry_class, article_entry_id FROM websites'
+websites_result = cur.execute(websites_query)
 
-df_columns = ['rss_url', 'article_entry_element', 'article_entry_class', 'article_entry_id']
-df = pd.DataFrame(SQL_Query, columns=df_columns)
-
-#iterate over websites
-for website, row in df.iterrows():
-    rss_url = row['rss_url']
-    tag = row['article_entry_element']
-    entry_class = row['article_entry_class']
-    entry_id = row['article_entry_id']
-    articles_data = parse_rss_feed(rss_url, tag, entry_class, entry_id)
+for row in websites_result:
+    id, rss_url, article_entry_element, article_entry_class, article_entry_id = row
+    articles_data = scrape_rss_feed(rss_url, article_entry_element, article_entry_class, article_entry_id)
     print(articles_data)
-#do something with article_data
+
 
 con.close()
