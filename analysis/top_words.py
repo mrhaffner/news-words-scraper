81740b98350021d0a844554e27ef475cb2a25b86
mrhaffner <mrhaffner13@gmail.com>
2021-11-27 19:51:15 -0600

    refactor: extracted get_words, and create_all_words_df from display_all_words

--- analysis/top_words.py
+++ analysis/top_words.py
@@ -3,25 +3,33 @@ import pandas as pd
 import texthero as hero
 
 class TopWords:
-    query = '''SELECT website_id, clean_title, clean_text, date, tag_left_wing, tag_right_wing, tag_state_funded
-                FROM articles AS a 
-                INNER JOIN websites AS w ON w.id = a.website_id;'''
-    base_df = pd.read_sql_query(query, db)
-
-
     def display_all_top_words():
-        print(f'Analyzing {TopWords.base_df.shape[0] - 1} News Articles...')
+        base_df = TopWords.get_words()
+
+        print(f'Analyzing {base_df.shape[0] - 1} News Articles...')
         print('... \n')
-        top_title_words = hero.top_words(TopWords.base_df['clean_title'])
-        top_text_words = hero.top_words(TopWords.base_df['clean_text'])
-        sum_all_words = top_text_words.add(top_title_words, fill_value=0).sort_values(ascending=False)
-        title_df = pd.DataFrame({'title_word': top_title_words.index, 'title_count': top_title_words.values})
-        text_df = pd.DataFrame({'text_word': top_text_words.index, 'text_count': top_text_words.values})
-        sum_df = pd.DataFrame({'sum_word': sum_all_words.index, 'sum_count': sum_all_words.values})
-        df = pd.concat([title_df, text_df, sum_df], axis=1)
-        df.drop
+        
+        df = TopWords.create_all_words_df(base_df)
         pd.options.display.float_format = '{:,.0f}'.format
         print(df.head(40))
 
 
+    def get_words():
+        query = 'SELECT website_id, clean_title, clean_text FROM articles'
+        return pd.read_sql_query(query, db)
 
+
+    def create_all_words_df(base_df):
+        top_title_words = hero.top_words(base_df['clean_title'])
+        top_text_words = hero.top_words(base_df['clean_text'])
+        sum_all_words = top_text_words.add(top_title_words, fill_value=0).sort_values(ascending=False)
+        title_df = pd.DataFrame({'title_word': top_title_words.index, 'title_count': top_title_words.values})
+        text_df = pd.DataFrame({'text_word': top_text_words.index, 'text_count': top_text_words.values})
+        sum_df = pd.DataFrame({'sum_word': sum_all_words.index, 'sum_count': sum_all_words.values})
+        return pd.concat([title_df, text_df, sum_df], axis=1)
+
+
+    # query = '''SELECT website_id, clean_title, clean_text, date, tag_left_wing, tag_right_wing, tag_state_funded
+    #             FROM articles AS a 
+    #             INNER JOIN websites AS w ON w.id = a.website_id;'''
+    # base_df = pd.read_sql_query(query, db)
