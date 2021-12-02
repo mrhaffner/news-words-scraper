5c03448dfc3d57b6d378ffdfaf8acda6eef51c83
mrhaffner <mrhaffner13@gmail.com>
2021-12-02 17:46:22 -0600

    feat: created save_graph_by_date_text_field which now can create scattertext files with various conditions today/alltime and article text or title

--- create_scattertext.py
+++ create_scattertext.py
@@ -1,10 +1,12 @@
 import scattertext as st    
 import pandas as pd
 from project import db
+import datetime
     
 
-def get_title_df():
-    query = '''
+def get_title_df(today=False):
+    today_sql = f" AND date = '{datetime.today().strftime('%Y-%m-%d')}'" if today else ''
+    query = f'''
         SELECT a.clean_title,
         CASE WHEN w.tag_right_wing = 1 THEN 'Right Wing'
             WHEN w.tag_left_wing = 1 THEN 'Left Wing'
@@ -12,8 +14,24 @@ def get_title_df():
         FROM articles AS a
         INNER JOIN websites AS w
         ON w.id = a.website_id
-        WHERE w.tag_right_wing = 1
-            OR w.tag_left_wing = 1
+        WHERE (w.tag_right_wing = 1
+            OR w.tag_left_wing = 1){today_sql};
+    '''
+    return pd.read_sql(query, db)
+
+
+def get_text_df(today=False):
+    today_sql = f" AND date = '{datetime.today().strftime('%Y-%m-%d')}'" if today else ''
+    query = f'''
+        SELECT a.clean_text,
+        CASE WHEN w.tag_right_wing = 1 THEN 'Right Wing'
+            WHEN w.tag_left_wing = 1 THEN 'Left Wing'
+        END AS political_leaning
+        FROM articles AS a
+        INNER JOIN websites AS w
+        ON w.id = a.website_id
+        WHERE (w.tag_right_wing = 1
+            OR w.tag_left_wing = 1){today_sql};
     '''
     return pd.read_sql(query, db)
 
@@ -43,11 +61,21 @@ def graph_text(df, analyze_col):
         transform=st.Scalers.dense_rank,
     )
     #variable name to write different input options to different files? today vs all etc
-    open("right_wing_vs_left_wing_news_words.html", "w").write(html)
+    return html
 
-
-df = get_title_df()
-graph_text(df, 'clean_title')
+def save_graph_by_date_text_field(text_field='title', today=False):
+    if text_field == 'title':
+        df = get_title_df(today)
+        html = graph_text(df, 'clean_title')
+        today_text = '_today' if today else ''
+        open(f"right_wing_vs_left_wing_title_words{today_text}.html", "w").write(html)
+    elif text_field == 'text':
+        df = get_text_df(today)
+        html = graph_text(df, 'clean_text')
+        today_text = '_today' if today else ''
+        open(f"right_wing_vs_left_wing_text_words{today_text}.html", "w").write(html)
+    else:
+        print('Invalid Input')
 
 
 db.close()
